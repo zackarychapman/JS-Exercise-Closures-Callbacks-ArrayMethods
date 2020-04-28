5f83c190a1b8c5a1f720b9528e68685c2f387233
zack <zackarychapman00@gmail.com>
2020-04-27 21:29:53 -0400

    passed first 2 tests.

--- index.js
+++ index.js
@@ -15,7 +15,8 @@
  * should return 'foofoo'.
 */
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+  // let n = stringList.length-1
+  return callback(stringList[stringList.length-1]);
 }
 
 // ⭐️ Example Challenge END ⭐️
@@ -48,8 +49,8 @@ function processFirstItem(stringList, callback) {
  * [2] Invoking `processLength` passing `[]` and `(num) => "There are " + num`,
  * should return "There are 0".
 */
-function processLength(/* CODE HERE */) {
-  /* CODE HERE */
+function processLength(list, callback) {
+  return callback(list.length);
 }
 
 /**
@@ -66,8 +67,8 @@ function processLength(/* CODE HERE */) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
-  /* CODE HERE */
+function processLastItem(stringList, callback) {
+  return callback(stringList.length[]-1);
 }
 
 /**
